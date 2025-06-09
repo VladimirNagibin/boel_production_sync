36067ab91290a0a80dab77f17a4d6e867fc7609a
Vladimir Nagibin <vlnagibin@yandex.ru>
2025-06-09 20:43:59 +0700

    Add infra

---
+++ bp_sync/src/core/settings.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):  # type: ignore
+    PROJECT_NAME: str = "bp_sync"
+    APP_RELOAD: bool = True
+    APP_HOST: str = "0.0.0.0"
+    APP_PORT: int = 8000
+    LOG_LEVEL: str = "INFO"
+
+    POSTGRES_HOST: str = "127.0.0.1"
+    POSTGRES_PORT: int = 5442
+    POSTGRES_USER: str = "postgres"
+    POSTGRES_PASSWORD: str = "postgres"
+    POSTGRES_DB: str = "bp_sync"
+    POSTGRES_DB_ECHO: bool = True
+
+    SECRET_KEY: str = "your-secret-key"
+    ALGORITHM: str = "HS256"
+    USER_ADMIN: str = "admin"
+    PASS_ADMIN: str = "pass"
+
+    BITRIX_LOGIN: str
+    BITRIX_PASS: str
+    BITRIX_CLIENT_ID: str
+    BITRIX_CLIENT_SECRET: str
+    BITRIX_PORTAL: str
+    BITRIX_REDIRECT_URI: str
+
+    BASE_DIR: str = str(Path(__file__).resolve().parent.parent)
+    LOGGING_FILE_MAX_BYTES: int = 500_000
+
+    @property
+    def dsn(self) -> str:
+        return (
+            f"postgresql+asyncpg://{self.POSTGRES_USER}:"
+            f"{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:"
+            f"{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        )
+
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8"
+    )
+
+
+settings = Settings()
---
+++ bp_sync/src/main.py
@@ -0,0 +1,51 @@
+from contextlib import asynccontextmanager
+from typing import AsyncIterator
+
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
+from sqladmin import Admin
+
+# from admin.admin_models import ProductAdmin, ProductHSAdmin
+# from admin.authenticate import BasicAuthBackend
+# from api.v1.health import health_router
+# from api.v1.products import product_router
+# from api.v1.products_hs import producths_router
+from core.logger import LOGGING, logger
+from core.settings import settings
+from db.postgres import engine
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncIterator[None]:
+    ...
+    yield
+    ...
+
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    docs_url="/api/openapi",
+    openapi_url="/api/openapi.json",
+    default_response_class=ORJSONResponse,
+    lifespan=lifespan,
+)
+
+# app.include_router(product_router, prefix="/api/v1/qr", tags=["qr"])
+# app.include_router(health_router, prefix="/api/v1/health", tags=["health"])
+# app.include_router(producths_router, prefix="/api/v1/hs", tags=["hs"])
+# auth_backend = BasicAuthBackend()
+# admin = Admin(app, engine, authentication_backend=auth_backend)
+# admin.add_view(ProductAdmin)
+# admin.add_view(ProductHSAdmin)
+
+if __name__ == "__main__":
+    logger.info("Start mark.")
+    uvicorn.run(
+        "main:app",
+        host=settings.APP_HOST,
+        port=settings.APP_PORT,
+        log_config=LOGGING,
+        log_level=settings.LOG_LEVEL.lower(),
+        reload=settings.APP_RELOAD,
+    )
