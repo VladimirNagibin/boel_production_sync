8bbf5b2705219e8c8b0c06bb1745a66faf1b3485
Vladimir Nagibin <vlnagibin@yandex.ru>
2025-09-24 17:28:27 +0700

    Upd migration managers id

--- bp_sync/src/migration/versions/2025_08_28_1432-bcf7b2395833_add_disk_id_in_model_manager.py
+++ bp_sync/src/migration/versions/2025_08_28_1432-bcf7b2395833_add_disk_id_in_model_manager.py
@@ -47,7 +47,7 @@ def upgrade() -> None:
     # ### end Alembic commands ###
     query = get_query_for_bulk_insert(
         "users",
-        ["user_id"],
+        ["external_id"],
         USERS_VALUES,
     )
     op.execute(sa.text(query))
